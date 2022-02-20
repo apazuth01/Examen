c961da61fba396d500c88ef85dc09cf7c7f24b2e
Tingo <86501910+apazuth01@users.noreply.github.com>
2022-02-19 22:19:50 -0600

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/examen/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/examen/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.examen", appContext.getPackageName());
+        assertEquals("com.example.PM1E13452", appContext.getPackageName());
     }
 }
